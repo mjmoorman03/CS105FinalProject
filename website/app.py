edc32830af49d34fe9e80ae4d1d1dd55e54a8226
mjmoorman03 <90151067+mjmoorman03@users.noreply.github.com>
2023-12-06 23:30:34 -0500

    Website should work now

--- website/app.py
+++ website/app.py
@@ -12,6 +12,14 @@ def match(person1: dict, person2: dict):
                 and person1['Year'] == person2['Year']
     return matched
 
+def matchWithBlanks(person1: dict, person2: dict):
+    ''' true iff fields match '''
+    # doesn't just check person1 == person2 bc person2 has a name field
+    matched = (person1['Concentration 1'] == person2['Concentration 1'] or not person1['Concentration 1']) \
+                and (person1['Concentration 2'] == person2['Concentration 2'] or not person1['Concentration 2']) \
+                and (person1['House'] == person2['House'] or not person1['House']) \
+                and (person1['Year'] == person2['Year'] or not person1['Year'])
+    return matched
 
 def identifyIndividual(person: dict):
     # returns a list of the names of the people who match the person
@@ -21,8 +29,8 @@ def identifyIndividual(person: dict):
     facebookFile = '../105facebook_cleaned.csv'
     f = open(facebookFile, 'r', encoding='utf-8-sig')
     data = list(csv.DictReader(f))
-
-    matches = [p['Name'] for p in data if match(person, p)]
+    matches = [p for p in data if matchWithBlanks(person, p)]
+    f.close()
     return matches
 
 @app.route('/', methods=['GET'])
@@ -41,13 +49,14 @@ def result():
     person = {
         'Concentration 1': concentration1,
         'Concentration 2': concentration2,
-        'House': house,
+        'House': house[:-6],
         'Year': year
     }
+    print(person)
     # Call the identifyIndividual() function
     matches = identifyIndividual(person)
     # Render the results template with the matches
-    return render_template('result.html', numMatches = len(matches))
+    return render_template('result.html', numMatches=str(len(matches)))
 
 if __name__ == '__main__':
     # Define your data
